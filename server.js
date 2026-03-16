e74ea680f9c0e60d32540031990906fae882d4de
bhargavverloop <verloop.dev6@gmail.com>
2026-03-16 15:09:51 +0530

    1.Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const next = require("next");
 const http = require("http");
 
-const dev = false;
-const app = next({ dev });
-const handle = app.getRequestHandler();
-
 const port = process.env.PORT || 3000;
+
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   http.createServer((req, res) => {
     handle(req, res);
   }).listen(port, () => {
-    console.log("Next.js running on port", port);
+    console.log("Server running on port " + port);
   });
 });
